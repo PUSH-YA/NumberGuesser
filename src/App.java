7e1812fb527ade52cdd5cce4ee5d7c824df0dd32
PUSH-YA <91928008+PUSH-YA@users.noreply.github.com>
2022-03-19 01:30:59 -0700

    made it better

--- src/App.java
+++ src/App.java
@@ -1,24 +1,29 @@
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class App {
     public static void main( String args []) {
         Random rand = new Random();
         int upperbound = 100;
          int int_random = rand.nextInt(upperbound); // generate random number from 1-100
-         System.out.println("the integer from 0 to " + (upperbound) + " is " + int_random);
+         //System.out.println("the integer from 0 to " + (upperbound) + " is " + int_random);
 
         Scanner my_obj = new Scanner(System.in); // create a scanner obj
-            System.out.println("Enter your guess from 1 to 100");
+            System.out.println("Enter your number higher than mine (from 1 to 100)");
 
             int guess = my_obj.nextInt(); // read user input
         
           my_obj.close();  
 
-        if(guess == int_random){
+        if(guess > int_random){
             System.out.println("Correct :)");
-        }else{
-            System.out.println("Loser!");
+        }else if (guess == int_random){
+            System.out.println("Same number, not sure how i feel about it :/");
+        }else
+        {
+            System.out.println("Loser :( ");
 
         }
     }
